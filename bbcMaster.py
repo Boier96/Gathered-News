7ed2486503daa7ee009ff64a09045df5607dda8e
Luminosity-incarnate <lumitorwegerif@gmail.com>
2026-05-06 14:08:54 +0200

    pushingtss

--- bbcMaster.py
+++ bbcMaster.py
@@ -2,41 +2,83 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+from bs4 import XMLParsedAsHTMLWarning
+import warnings
+import logging
+
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 class Article:
   article_paragraphs = {}
   article_headers = {}
   article_title = ""
 
-def createArticle(targeturl):
-  article_html = requests.get(targeturl)
-  article_soup = BeautifulSoup(article_html, 'html.parser')
-  new_article = Article()
-  new_article.article_headers = article_soup.find('article').find_all('h2')
-  new_article.article_title = article_soup.find('article').find_all('h1')
-  new_article.article_paragraphs = article_soup.find('article').find_all('p')
-  return new_article
+class ReturnURL:
+  url = ""
+  failed = False
 
-def getFormattedUrl(href_string, top_url):
-  new_string = re.findall('"([^"]*)"', href_string)[0]
-  if top_url not in new_string:
-    new_string = top_url + new_string
-  return new_string
+def createArticle(targeturl):
+  article_html = requests.get(targeturl, headers={'User-Agent': 'Custom'})
+  try:
+    article_html.raise_for_status()
+    article_soup = BeautifulSoup(article_html.text, 'html.parser')
+    new_article = Article()
+    try:
+      new_article.article_headers = article_soup.find_all('h2')
+      new_article.article_title = article_soup.find_all('h1').text
+      new_article.article_paragraphs = article_soup.find_all('p')
+    except:
+      print(article_soup.find('h1').text)
+
+    return new_article
+  except:
+    try:
+      article_soup = BeautifulSoup(article_html.text, 'lxml')
+      new_article = Article()
+      try:
+        new_article.article_headers = article_soup.find_all('h2')
+        new_article.article_title = article_soup.find_all('h1').text
+        new_article.article_paragraphs = article_soup.find_all('p')
+      except:
+        print(article_soup.find('h1').text)
+
+      return new_article
+    except:
+      print("returning shit for: " + targeturl)
+      return Article()
+
+def getFormattedUrl(href_string):
+  result = ReturnURL()
+  #print(href_string)
+  pattern =  r'https?://\S+|www\.\S+'
+  result.url = re.search(pattern, href_string)[0].replace('"', '')
+  print(result.url)
+  if "bbc.com" not in result.url:
+    result.failed = True
+    print("cooked")
+    return result
+  print("returning proper result with url:" + result.url)
+  return result
 
 def scrapetheBBC(targetFile):
-  print("hello")
   target_file = targetFile
   url = "https://www.bbc.com/"
   full_html = requests.get(url)
-  soup = BeautifulSoup(full_html, 'html.parser')
+  soup = BeautifulSoup(full_html.text, 'html.parser')
 
-  article_area = soup.find(class_="sc-cd6075cf-0 cJhFtM")
+  article_area = soup.find(class_="sc-9cd5bb24-0 cAWTFK")
 
   articles_finished = []
 
   article_links = article_area.find_all('a')
+
   for link in article_links:
-    print(link.text)
-    articles_finished.append(createArticle(getFormattedUrl(link.prettify(), "https://bbc.com")))
+    try:
+      returnedUrl = getFormattedUrl(link.prettify) 
+      if not returnedUrl.failed:
+        print("gurt")
+        articles_finished.append(createArticle(getFormattedUrl(link.prettify()).url))
+    except:
+      continue
   for article in articles_finished:
     print(article.article_title)
